45d1dfc44f08c0489c626030e7421f010cc25e59
lucachiossi <engr.luca.chiossi@gmail.com>
2021-03-29 16:25:35 +0200

    added Question and Choice to admin.py with needed inline presentation details

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
@@ -10,9 +9,19 @@ class LessonInline(admin.StackedInline):
     extra = 5
 
 
+class QuestionInLine(admin.StackedInline):
+    model = Question
+    extra = 3
+
+
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 4
+
+
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+    inlines = [LessonInline, QuestionInLine]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
@@ -22,9 +31,19 @@ class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
 
-# <HINT> Register Question and Choice models here
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInLine]
+    list_display = ['question_text']
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['choice_text']
+
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
+admin.site.register(Submission)
